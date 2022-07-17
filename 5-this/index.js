7556e934a32b8e7109c259d1901c7d102c9bfda1
ingporto <ing.portocarrero@gmail.com>
2022-07-17 18:26:02 -0500

    this

--- 5-this/index.js
+++ 5-this/index.js
@@ -3,14 +3,28 @@ function showFullName(){
 }
 
 const user = {
-    name: "ryan", //property
-    lastname: "ray",
+    name: "Juan", //property
+    lastname: "Perez",
     age: 38,
-    showFullName: showFullName,
-    showAge(){
-        return 38
+    showFullName(){
+        return (this.name + " " + this.lastname).trim()
+    },
+    account: {
+        number: "",
+        amount: 100,
+        deposit(quantity){
+            this.amount = this.amount + quantity
+        },
+        withdrawl(quantity){
+            this.amount = this.amount - quantity
+        }
     }
 }
 
-console.log(user.showFullName())
-console.log(user.showAge())
+console.log(user.account.amount)
+user.account.deposit(100)
+
+console.log(user.account)
+user.account.withdrawl(50)
+
+console.log(user.account)
